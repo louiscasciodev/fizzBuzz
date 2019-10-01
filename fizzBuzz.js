17cbca628edc1c5ca30eb4bd9715b6546993021a
JennyViannay <jenny.viannay75@gmail.com>
2019-10-01 09:22:16 +0200

    dojo fizzbuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,15 @@
+//Écrire un programme qui renvoie :
+
+       	//1 - "fizz" dans le cas où le nombre passé en paramètre est un multiple de 3
+       	//2 - "buzz" dans le cas où le nombre passé en paramètre est un multiple de 5
+       	//3 - "fizzbuzz" dans le cas où le nombre passé est à la fois un multiple de 3 et de 5
+       	//4 - Le nombre dans le cas où ce dernier n’est ni un multiple de 3 ni un multiple de 5
+
+
+//module.exports = 
+
+function fizzBuzz(val) {
+    console.log("your code here");
+}  
+
+fizzBuzz(15);
